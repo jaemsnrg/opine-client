df7efa0096115c650eb20be2d33d890dbc8d56eb
“jaemsnrg” <jaemsnrg@gmail.com>
2023-05-23 10:41:22 +0200

    rounding off

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,14 +1,14 @@
-import { Layout } from '@/components/dom/Layout'
-import '@/global.css'
+import { Layout } from "@/components/dom/Layout";
+import "@/global.css";
 
 export const metadata = {
-  title: 'Next.js + Three.js',
-  description: 'A minimal starter for Nextjs + React-three-fiber and Threejs.',
-}
+  title: "Opine furniture",
+  description: "Timeless design and practical ",
+};
 
 export default function RootLayout({ children }) {
   return (
-    <html lang='en' className='antialiased'>
+    <html lang="en" className="antialiased">
       {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
@@ -19,5 +19,5 @@ export default function RootLayout({ children }) {
         <Layout>{children}</Layout>
       </body>
     </html>
-  )
+  );
 }
--- src/components/canvas/Models.jsx
+++ src/components/canvas/Models.jsx
@@ -1,57 +1,61 @@
-import { useState, useRef, useEffect } from 'react'
-import { useGLTF } from '@react-three/drei'
-import { useFrame, useThree } from '@react-three/fiber'
+import { useState, useRef, useEffect } from "react";
+import { useGLTF } from "@react-three/drei";
+import { useFrame, useThree } from "@react-three/fiber";
 
 export function Table(props) {
-  const { camera } = useThree()
-  const [isFocused, setIsFocused] = useState(false)
-  const { scene } = useGLTF('/models/table.glb')
-  const modelRef = useRef()
-  const zoomSpeed = 0.04
-  const prevRotationYRef = useRef(0)
+  const { camera } = useThree();
+  const [isFocused, setIsFocused] = useState(false);
+  const { scene } = useGLTF("/models/table.glb");
+  const modelRef = useRef();
+  const zoomSpeed = 0.04;
+  const prevRotationYRef = useRef(0);
 
-  useEffect(() => {
-    modelRef.current.position.z = -2 // Initial zoomed-in position
-    modelRef.current.position.y = -1 // Initial zoomed-in position
-  }, [])
+  // useEffect(() => {
+  //   modelRef.current.position.z = -2 // Initial zoomed-in position
+  //   modelRef.current.position.y = -1 // Initial zoomed-in position
+  // }, [])
 
-  useFrame(({ clock }) => {
-    const time = clock.getElapsedTime()
+  // useFrame(({ clock }) => {
+  //   const time = clock.getElapsedTime()
 
-    // Zoom effect
-    if (!isFocused) {
-      if (modelRef.current.position.z < 6) {
-        const distanceToTarget = 2 - modelRef.current.position.z
-        const zoomDistance = distanceToTarget * zoomSpeed
+  //   // Zoom effect
+  //   if (!isFocused) {
+  //     if (modelRef.current.position.z < 6) {
+  //       const distanceToTarget = 2 - modelRef.current.position.z
+  //       const zoomDistance = distanceToTarget * zoomSpeed
 
-        if (zoomDistance > 0.001) {
-          modelRef.current.position.z += zoomDistance
-        }
-      }
+  //       if (zoomDistance > 0.001) {
+  //         modelRef.current.position.z += zoomDistance
+  //       }
+  //     }
 
-      if (modelRef.current.position.y < 4) {
-        const distanceToTarget = -0.5 - modelRef.current.position.y
-        const zoomDistance = distanceToTarget * zoomSpeed
+  //     if (modelRef.current.position.y < 4) {
+  //       const distanceToTarget = -0.5 - modelRef.current.position.y
+  //       const zoomDistance = distanceToTarget * zoomSpeed
 
-        if (zoomDistance > 0.001) {
-          modelRef.current.position.y += zoomDistance
-        }
-      }
+  //       if (zoomDistance > 0.001) {
+  //         modelRef.current.position.y += zoomDistance
+  //       }
+  //     }
 
-      // Rotation effect
-      if (!isFocused) {
-        modelRef.current.rotation.y = prevRotationYRef.current + time * 0.1
-      }
-    }
+  //     // Rotation effect
+  //     if (!isFocused) {
+  //       modelRef.current.rotation.y = prevRotationYRef.current + time * 0.1
+  //     }
+  //   }
 
-    if (isFocused) {
-      prevRotationYRef.current = modelRef.current.rotation.y
-    }
-  })
+  //   if (isFocused) {
+  //     prevRotationYRef.current = modelRef.current.rotation.y
+  //   }
+  // })
 
   return (
     <mesh ref={modelRef}>
-      <primitive onPointerDown={() => setIsFocused(true)} object={scene} {...props} />
+      <primitive
+        onPointerDown={() => setIsFocused(true)}
+        object={scene}
+        {...props}
+      />
     </mesh>
-  )
+  );
 }
